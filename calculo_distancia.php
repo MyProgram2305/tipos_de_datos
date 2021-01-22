999aa987ba7c33170ff09554ede624fb6dcbecc6
myprogram2305 <75906602+MyProgram2305@users.noreply.github.com>
2021-01-22 16:08:36 -0600

    calcular la distacia o el tiempo

---
+++ calculo_distancia.php
@@ -0,0 +1,23 @@
+<!--
+	Pablo Miguel Velasco Garcia 4°H
+-->
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+
+<?php
+
+$velocidad = 50;
+$distancia = 0;
+$tiempo = 60;
+
+echo "<h1>Calcula la distancia </h1>";
+$distancia = $velocidad * $tiempo;
+echo"La distancia recorrida en 60 min con una velocidad de 50 km por hora es :" . $distancia. "m/hr"  .   " <br> ";
+?>
+			<img src="img/taco.jpeg" width="50">
+</body>
+</html>
---
+++ calculo_velocidad.php
@@ -0,0 +1,27 @@
+<!--
+	Pablo Miguel Velasco Garcia 4°H
+-->
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+
+<?php
+
+$velocidad = 0;
+$distancia = 2000;
+$tiempo = 20;
+
+echo "<h1>Calcula de la velocidad </h1>";
+ 
+ $velocidad = $distancia / $tiempo;
+ echo "se alcanza una distancia de 1000 mts en un tiempo de 20 min a una velocidad de :" . "km/hr" . "<br>  " ;
+
+ //echo sqrt(9) . "<br> ";
+ // echo (10 % 100) . "<br>  ";
+?>
+			<img src="img/taco.jpeg" width="50">
+</body>
+</html>
